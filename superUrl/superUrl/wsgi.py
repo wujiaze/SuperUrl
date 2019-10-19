24d6ab514c30ffdbc81eb5c845f2380941038a55
wujiaze <1051277749@qq.com>
2019-10-19 19:29:26 +0800

    1

--- superUrl/superUrl/wsgi.py
+++ superUrl/superUrl/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for SuperUrl project.
+WSGI config for superUrl project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SuperUrl.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superUrl.settings")
 
 application = get_wsgi_application()
