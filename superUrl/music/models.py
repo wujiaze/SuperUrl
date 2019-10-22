ebf67a4d3d2f2199c78c058a002785bbe1c0f607
Tedu <tjs_1997o@163.com>
2019-10-22 19:54:21 +0800

    音乐数据库表创建

--- superUrl/music/models.py
+++ superUrl/music/models.py
@@ -2,3 +2,37 @@ from django.db import models
 
 # Create your models here.
 
+
+class MusicInformation(models.Model):
+    name = models.CharField(max_length=100,null=False,verbose_name='音乐名')
+    download_count = models.IntegerField(default=0,verbose_name='下载量')
+    star_one = models.IntegerField(default=0,verbose_name='一星数')
+    star_two = models.IntegerField(default=0,verbose_name='二星数')
+    star_three = models.IntegerField(default=0,verbose_name='三星数')
+    star_four = models.IntegerField(default=0,verbose_name='四星数')
+    star_five = models.IntegerField(default=0,verbose_name='五星数')
+    star_avg = models.FloatField(default=0,verbose_name='平均星数')
+    create_time = models.DateTimeField(auto_now_add=True,verbose_name='创建时间')
+    update_time = models.DateTimeField(auto_now=True,verbose_name='更新时间')
+    url = models.CharField(max_length=1000,null=False,verbose_name='音乐资源链接')
+
+    class Meta:
+        db_table = 'music_information'
+
+
+
+
+class MusicKeyword(models.Model):
+    key_word = models.CharField(max_length=50,null=False,unique=True,verbose_name='关键字')
+    create_time = models.DateTimeField(auto_now_add=True,verbose_name='创建时间')
+
+    musicinformation = models.ManyToManyField(MusicInformation)
+
+    class Meta:
+        db_table = 'music_keyword'
+
+
+
+
+
+
--- superUrl/superUrl/settings.py
+++ superUrl/superUrl/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'superUrl',
         'USER': 'root',
-        'PASSWORD': '83117973bb',
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
