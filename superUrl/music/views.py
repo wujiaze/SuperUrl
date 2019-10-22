307d76a10b6641c0a897048f7959abb8cf12d3f0
Tedu <tjs_1997o@163.com>
2019-10-22 20:33:25 +0800

    music.views

--- superUrl/music/views.py
+++ superUrl/music/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -8,7 +9,19 @@ def search_music(request):
     if request.method == 'GET':
         operation = request.GET.get('operation')
         keyword = request.GET.get('keyword')
-        
+        print(operation)
+        print(keyword)
+        if operation == 0:
+            #todo 爬虫接口
+            pass
+        elif operation == 1:
+
+
+
+
+
+
+        return JsonResponse({'code':200})
 
 
 
