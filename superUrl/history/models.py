9e1a837db6809f22583e03ccd03f72245301ce45
Tedu <tjs_1997o@163.com>
2019-10-24 21:10:24 +0800

    music

--- superUrl/history/models.py
+++ superUrl/history/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from user.models import UserProfile
 # Create your models here.
+
+
+class history(models.Model):
+    keyword = models.CharField(max_length=20,verbose_name='查询关键字')
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    userprofile = models.ForeignKey(UserProfile)
+
+    class Meta:
+        db_table = 'history'
--- superUrl/music/views.py
+++ superUrl/music/views.py
@@ -2,6 +2,7 @@ import redis
 from django.http import JsonResponse
 from django.shortcuts import render
 from music.models import MusicInformation,MusicKeyword
+import json
 # Create your views here.
 
 
@@ -20,45 +21,23 @@ def search_music(request):
         r = redis.Redis(host='127.0.0.1', port=6379, db=2)
 
         if r.exists(keyword):
-            data_list = r.lrange(keyword,0,-1)
-            all_list = []
-            for data in data_list:
-                data_dict = {}
-                data = data.decode()
-                name = data.split(',')[1]
-                download_count = data.split(',')[2]
-                star_one = data.split(',')[3]
-                star_two = data.split(',')[4]
-                star_three = data.split(',')[5]
-                star_four = data.split(',')[6]
-                star_five = data.split(',')[7]
-                stat_avg = data.split(',')[8]
-                url = data.split(',')[9]
-                data_dict['name'] = name
-                data_dict['download_count'] = download_count
-                data_dict['star_one'] = star_one
-                data_dict['star_two'] = star_two
-                data_dict['star_three'] = star_three
-                data_dict['star_four'] = star_four
-                data_dict['star_five'] = star_five
-                data_dict['star_avg'] = stat_avg
-                data_dict['url'] = url
-                data_list.append(data_dict)
+            keyword = "info:music:" + keyword
+            res = r.get(keyword)
+            res_list = json.loads(res.decode())
 
             res = {
                 'code':200,
-                'data':all_list
+                'data':res_list
             }
 
             return JsonResponse(res)
 
-
         else:
             try:
+                print('redis不存在')
                 kw = MusicKeyword.objects.get(keyword=keyword)
                 info_list = kw.musicinformation.all()
                 all_list = []
-                redis_list = []
                 for item in info_list:
                     data_dict = {}
                     data_dict['name'] = item.name
@@ -68,25 +47,13 @@ def search_music(request):
                     data_dict['star_three'] = item.star_three
                     data_dict['star_four'] = item.star_four
                     data_dict['star_five'] = item.star_five
-                    data_dict['star_avg'] = item.stat_avg
+                    data_dict['star_avg'] = item.star_avg
                     data_dict['url'] = item.url
                     all_list.append(data_dict)
-                    info = [str(item.name),
-                            str(item.download_count),
-                            str(item.star_one),
-                            str(item.star_two),
-                            str(item.star_three),
-                            str(item.star_four),
-                            str(item.star_five),
-                            str(item.stat_avg),
-                            str(item.url)]
-                    res = ','.join(info)
-                    redis_list.append(res)
-                r.lpush(keyword,redis_list)
 
-                #取出来的值的格式(result) -> r.lrange(keyword,0,-1)  ->  [b"['aaaa', 'bbbbbb', 'cccccccc']"]
-                #type(result)  -> list
-                #resutl[0].decode()   ->    str
+                str_list = str(json.dumps(all_list))
+                keyword = "info:music:" + keyword
+                r.set(keyword,str_list)
 
                 result = {
                     'code': 200,
@@ -98,9 +65,11 @@ def search_music(request):
 
 
             except Exception as e:
-
+                print('都不存在')
             #todo 爬虫接口
-            #  得到　　data
+            # 爬虫存到数据库
+
+
 
                 data = [{'name':'aaa1','url':'xxx1'},
                         {'name':'aaa2','url':'xxx2'},
@@ -133,7 +102,7 @@ def search_music(request):
                     data_dict['star_three'] = item.star_three
                     data_dict['star_four'] = item.star_four
                     data_dict['star_five'] = item.star_five
-                    data_dict['star_avg'] = item.stat_avg
+                    data_dict['star_avg'] = item.star_avg
                     data_dict['url'] = item.url
                     all_list.append(data_dict)
                     info = [str(item.name),
@@ -143,7 +112,7 @@ def search_music(request):
                             str(item.star_three),
                             str(item.star_four),
                             str(item.star_five),
-                            str(item.stat_avg),
+                            str(item.star_avg),
                             str(item.url)]
                     res = ','.join(info)
                     redis_list.append(res)
@@ -153,6 +122,8 @@ def search_music(request):
                     'code': 200,
                     'data': all_list
                 }
+                # result = {'code':20000,
+                #           'error':'暂无资源'}
 
                 return JsonResponse(result)
 
@@ -160,24 +131,35 @@ def search_music(request):
 
 
 def get_history(request):
-    pass
+
+    return JsonResponse({'code':20000,
+                         'error':'未完成'})
+
 
 
 
 
 def add_download(request):
-    pass
+
+    return JsonResponse({'code':20000,
+                         'error':'未完成'})
+
 
 
 
 
 def get_rank(request):
-    pass
+
+
+    return JsonResponse({'code':20000,
+                         'error':'未完成'})
+
 
 
 
 def get_keylist(request):
-
+    return JsonResponse({'code':20000,
+                         'error':'未完成'})
     # todo 步骤1 查询redis
     # todo 步骤2 查询mysql 更新redis
     # todo 步骤3 交给爬虫 结束
@@ -185,7 +167,7 @@ def get_keylist(request):
     # todo 爬虫接口
     # 调用爬虫接口获取实时keylist
 
-    pass
+
 
 
 
